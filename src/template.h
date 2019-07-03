91fab2251285b67914eb405b025b74aff6d6dacd
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-07-03 15:17:35 +0300

    add own template

---
+++ src/template.h
@@ -0,0 +1,20 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __TEMPLATE_H
+#define __TEMPLATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported function prototypes --------------------------------------------- */
+/* Exported functions ------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__TEMPLATE_H
